6f3d565dac20cab46ceaed8bd9e6c288e6495ae7
inb <inorton@gmail.com>
2019-02-05 17:11:51 +0000

    initial commit

--- clspy/Program.cs
+++ clspy/Program.cs
@@ -1,15 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
 
 namespace clspy
 {
-    class Program
+    sealed class Program
     {
+        static Mutex sync = null;
+
+        static void Lock()
+        {
+            do
+            {
+                try
+                {
+                    try
+                    {
+                        sync = Mutex.OpenExisting("clspy");
+                    }
+                    catch
+                    {
+                        sync = new Mutex(false, "clspy");
+                    }
+                }
+                catch
+                {
+
+                }
+            } while (sync == null);
+
+            while (!sync.WaitOne());
+        }
+
+        static void Unlock()
+        {
+            sync.ReleaseMutex();
+        }
+
+        static void SaveLog(string workdir, string args, DateTime start, DateTime ended)
+        {
+            Lock();
+            try
+            {
+                var filename = "c:\\clspy.log";
+                File.AppendAllText(filename, String.Format("{},{},{},{}", start, ended, workdir, args));
+            }
+            finally
+            {
+                Unlock();
+            }
+        }
+
         static void Main(string[] args)
         {
+            var real_cl = Environment.GetEnvironmentVariable("REAL_CL_EXE");
+            var started = DateTime.Now;
+
+            var proc = new Process();
+            proc.StartInfo = new ProcessStartInfo(real_cl);
+
+            proc.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+            proc.StartInfo.Arguments = Environment.CommandLine;
+            try
+            {
+                foreach (String name in Environment.GetEnvironmentVariables().Keys)
+                {
+                    proc.StartInfo.EnvironmentVariables[name] = Environment.GetEnvironmentVariable(name);
+                }
+
+                proc.Start();
+                proc.WaitForExit();
+                var ended = DateTime.Now;
+                SaveLog(Environment.CurrentDirectory, Environment.CommandLine, started, ended);
+                Environment.Exit(proc.ExitCode);
+            } catch (Exception err) {
+                Console.Error.WriteLine(err.Message);
+                Environment.Exit(1);
+            }
         }
     }
 }
